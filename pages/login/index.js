2d2273df4a2c5fe4b323bb1f1ad450ea17fae402
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2022-06-15 01:08:58 +0800

    test verification

--- pages/login/index.js
+++ pages/login/index.js
@@ -19,6 +19,27 @@ const goToMainPage = () => {
         <div className={styles['app-logo-container']}>
           <img src="" alt="App Logo" />
         </div>
+        <div>
+          <div className={styles['input-nohp']}>
+            <label htmlFor="phone-number">Silahkan masukan nomor ponsel anda</label>
+            <input type="number" id='phone-number' />
+          </div>
+          <div>
+            <p>Kami perlu memverifikasi bahwa nomor ini benar milik anda</p>
+            <div>
+              <input type="radio" name="metode-verifikasi" value="wa" id="metode-wa" />
+              <label htmlFor="metode-wa">Whatsapp</label>
+            </div>
+            <div>
+              <input type="radio" name="metode-verifikasi" value="sma" id="metode-sms" defaultChecked />
+              <label htmlFor="metode-sms">SMS</label>
+            </div>
+          </div>
+        </div>
+        <div className={styles['login-as-container']}>
+          <p>Login Sebagai</p>
+          <div className={styles['garis']}></div> 
+        </div>
         <div className={styles['buttons-container']}>
           <ul>
             <li>
