a05f957a165c86e010be7656390e03c437baab5e
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2022-06-20 22:28:11 +0800

    finish

--- components/ApplicationBrand/ApplicationBrand.jsx
+++ components/ApplicationBrand/ApplicationBrand.jsx
@@ -3,7 +3,7 @@ import styles from './ApplicationBrand.module.css';
 export default function ApplicationBrand({ myStyle }) {
   return (
     <h2 className={styles['main-container']} style={myStyle}>
-      Nama Aplikasi
+      Callus
     </h2>
   )
 }
